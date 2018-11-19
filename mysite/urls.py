de08b2f580213b97adba5fd9da06608aa69db1e2
Your Name <sanjay7m@gmail.com>
2018-11-19 23:22:34 +0530

    My Django Girls app, first commit

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path
-from blog import views
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
+    path('admin/', admin.site.urls),
+    path('', include('blog.urls')),
 ]
